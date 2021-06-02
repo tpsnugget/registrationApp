a831fd0ba39c7612ff265eee894570ad90cecfa9
Mike Giebner <mikegiebner@gmail.com>
2021-06-01 22:16:38 -0400

    Add remaining text fields to Registration form

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -9,8 +9,29 @@ import Typography from '@material-ui/core/Typography'
 
 const Registration = () => {
 
+  const [ address1, setAddress1 ] = useState('')
+  const [ address2, setAddress2 ] = useState('')
+  const [ city, setCity ] = useState('')
+  const [ country, setCountry ] = useState('')
   const [ firstName, setFirstName ] = useState('')
   const [ lastName, setLastName ] = useState('')
+  const [ usState, setUsState ] = useState('')
+  const [ zip, setZip ] = useState('')
+
+  const handleAddress1Change = (e) => {
+    setAddress1(e.target.value)
+  }
+
+  const handleAddress2Change = (e) => {
+    setAddress2(e.target.value)
+  }
+  const handleCityChange = (e) => {
+    setCity(e.target.value)
+  }
+
+  const handleCountryChange = (e) => {
+    setCountry(e.target.value)
+  }
 
   const handleFirstNameChange = (e) => {
     setFirstName(e.target.value)
@@ -19,16 +40,30 @@ const Registration = () => {
   const handleLastNameChange = (e) => {
     setLastName(e.target.value)
   }
+  const handleUsStateChange = (e) => {
+    setUsState(e.target.value)
+  }
+
+  const handleZipChange = (e) => {
+    setZip(e.target.value)
+  }
 
   const handleSubmit = () => {
     const userData = {
       firstName,
       lastName
     }
-    // axios.post('https://rainbow-river-4709.herokuapp.com/register')
-    axios.post('http://localhost:3001/register', userData)
+
+    axios.post('https://rainbow-river-4709.herokuapp.com/register', userData)
+    // axios.post('http://localhost:3001/register', userData)
       .then( res => {
-        console.log('Registration res.data is ', res.data)
+        console.log('Registration register POST res.data is ', res.data)
+      })
+    
+    axios.get('https://rainbow-river-4709.herokuapp.com/adminReport')
+    // axios.get('http://localhost:3001/adminReport')
+      .then( res => {
+        console.log('Registration adminReport GET res.data is ', res.data)
       })
   }
 
@@ -49,6 +84,30 @@ const Registration = () => {
             </Grid>
             <Grid item xs={3}>
               <TextField label='Last Name' value={lastName} variant='outlined' onChange={handleLastNameChange} style={{width: '100%', marginLeft: '0.5rem'}} />
+            </Grid>
+          </Grid>
+
+          <Grid container style={{marginTop: '1rem'}}>
+            <Grid item xs={4}>
+              <TextField label='Address 1' value={address1} variant='outlined' onChange={handleAddress1Change} style={{width: '100%'}} />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField label='Address 2' value={address2} variant='outlined' onChange={handleAddress2Change} style={{width: '100%', marginLeft: '0.5rem'}} />
+            </Grid>
+          </Grid>
+
+          <Grid container style={{marginTop: '1rem'}}>
+            <Grid item xs={3}>
+              <TextField label='City' value={city} variant='outlined' onChange={handleCityChange} style={{width: '100%'}} />
+            </Grid>
+            <Grid item xs={2}>
+              <TextField label='State' value={usState} variant='outlined' onChange={handleUsStateChange} style={{width: '100%', marginLeft: '0.5rem'}} />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField label='Zip Code 12345 or 12345-1234' value={zip} variant='outlined' onChange={handleAddress1Change} style={{width: '100%', marginLeft: '0.5rem'}} />
+            </Grid>
+            <Grid item xs={2}>
+              <TextField label='Country' value={country} variant='outlined' onChange={handleCountryChange} style={{width: '100%', marginLeft: '0.5rem'}} />
             </Grid>
           </Grid>
 
